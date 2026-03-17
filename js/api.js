40b29aabe0f9d26d90a2890c27e99f191e2d38a0
Eduardo <duduoliveirafelipe@gmail.com>
2026-03-17 20:20:31 -0300

    UPDATE - arrumando os js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // URL base da API
-const API_URL = 'http://localhost:5000/api'
+const API_URL = '/api'
 
 // Função auxiliar para fazer requisições
 async function makeRequest(endpoint, method = 'GET', data = null) {
--- js/auth.js
+++ js/auth.js
@@ -16,7 +16,7 @@ async function cadastrar() {
   }
 
   try {
-    const response = await fetch('http://localhost:5000/api/auth/register', {
+    const response = await fetch('/api/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -61,7 +61,7 @@ async function login() {
   }
 
   try {
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch('/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
